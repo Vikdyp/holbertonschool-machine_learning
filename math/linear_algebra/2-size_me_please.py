f5940afb7831ba82f72dd820dbe15218ffca2f00
Vikdyp <victor@roubin.net>
2025-12-03 11:45:18 +0100

    ajout docstring

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""module quuui contient la fonction pour calculer la forme d'une matrice"""
+
+
 def matrix_shape(matrix):
     """calculate the shape of a matrix"""
     shape = []
