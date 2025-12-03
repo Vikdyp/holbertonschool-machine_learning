912fe4477be9c586e97bf0f7958e99341f3e1354
Vikdyp <victor@roubin.net>
2025-12-03 13:23:51 +0100

    ajout docstring

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""module qui contient la fonction matrix_transpose"""
+
+
 def matrix_transpose(matrix):
+    """fonction qui permet de retourner la transposer d'une matrice 2D"""
     new_matrix = []
     for j in range(len(matrix[0])):
         row = []
