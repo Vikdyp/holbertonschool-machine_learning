d08c45bf8b8ebf2e47a6f6d410de64744a1e1cc6
Vikdyp <victor@roubin.net>
2025-12-03 13:21:52 +0100

    correction d'indentation

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
-    new_martix = []
+    new_matrix = []
     for j in range(len(matrix[0])):
         row = []
         for i in range(len(matrix)):
             row.append(matrix[i][j])
-            new_martix.append(row)
-    return new_martix
+        new_matrix.append(row)
+    return new_matrix
