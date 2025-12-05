58604a73c75453f2bce2b523ee0170bfe0ddd63e
Vikdyp <victor@roubin.net>
2025-12-05 18:35:26 +0100

    ajoyt des ticks

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""module that provide the frequency function"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def frequency():
-    """function that print an histogram"""
+
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.xticks(range(0, 101, 10))
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
     plt.xlim(0, 100)
     plt.ylim(0, 30)
     plt.show()
+
+frequency()
