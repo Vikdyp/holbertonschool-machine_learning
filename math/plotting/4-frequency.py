e449d1229c2dd66783ab17f4c578caaac21c3cd5
Vikdyp <victor@roubin.net>
2025-12-05 18:01:13 +0100

    correction limite x, y

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,5 +12,7 @@ def frequency():
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+    plt.hist(student_grades, bins=10, edgecolor='black')
+    plt.xlim(0, 100)
+    plt.ylim(0, 30)
     plt.show()
