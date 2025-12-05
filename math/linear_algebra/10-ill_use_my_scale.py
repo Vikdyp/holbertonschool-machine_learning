a7475376b1c54189a6f9aa8693dcebb6f536e0f4
Vikdyp <victor@roubin.net>
2025-12-05 14:08:42 +0100

    I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a function that returns
+the shape of a numpy.ndarray.
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray
+    """
+    return matrix.shape
